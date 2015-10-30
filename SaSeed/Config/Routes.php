dbc4917c83c1c1baefe1e4e89320b27a50fdedec
unknown <vsde@PCSOR242.gft.com>
2015-10-30 11:07:27 -0200

    Template realocation

--- SaSeed/Config/Routes.php
+++ SaSeed/Config/Routes.php
@@ -19,6 +19,6 @@ $path = dirname(__FILE__);
 $basePath = substr($path, 0, strpos($path, 'SaSeed'));
 define('ConfigPath', $basePath.'SaSeed'.DIRECTORY_SEPARATOR.'Config'.DIRECTORY_SEPARATOR);
 define('ViewPath', $basePath.'Application'.DIRECTORY_SEPARATOR.'View'.DIRECTORY_SEPARATOR);
-define('TemplatesPath', $basePath.'Application'.DIRECTORY_SEPARATOR.'View'.DIRECTORY_SEPARATOR.'Template'.DIRECTORY_SEPARATOR);
+define('TemplatesPath', $basePath.'Application'.DIRECTORY_SEPARATOR.'View'.DIRECTORY_SEPARATOR.'Templates'.DIRECTORY_SEPARATOR);
 define('GeneralJSPath', ViewPath.'Js'.DIRECTORY_SEPARATOR);
 define('GeneralCSSPath', ViewPath.'Css'.DIRECTORY_SEPARATOR);
